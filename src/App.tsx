dea9bfa4ce9d08f23171de42184be4d13a123684
eliya7870-oss <eliya7870@gmail.com>
2025-11-04 13:06:38 +0200

    vite scaffold and basic tile

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Tile from "./components/Tile/Tile";
+
+function App() {
+  return (
+    <>
+      <Tile />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Tile/Tile.tsx
@@ -0,0 +1,6 @@
+import "./Tile.css";
+
+function Tile() {
+  return <div className="tile"></div>;
+}
+export default Tile;
